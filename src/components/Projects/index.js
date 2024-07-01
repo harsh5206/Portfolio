9addaf26da772cf570dee571c3151110e93b860f
harsh <harshmangukiya2000@gmail.com>
2024-07-01 01:00:16 -0400

    abc remove

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -122,7 +122,7 @@ const Projects = ({openModal,setOpenModal}) => {
     return (
       <Container id="projects">
         <Wrapper>
-          <Title>Projects Abc</Title>
+          <Title>Projects</Title>
           <Desc>
             I have worked on a wide range of projects. From web apps to android apps. Here are some of my projects.
           </Desc>
